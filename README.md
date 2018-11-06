ba479b913cb9082932b4fcbb428b750aa700beef
Chris Hitchcott <hitchcott@gmail.com>
2018-11-06 13:40:00 +0700

    add boilerplate for react -> webpack -> inline html

---
+++ README.md
@@ -0,0 +1,8 @@
+# Webpack React Boilerplate
+
+### Yet Another Ultra-Minimal React Boilerplate Project
+
+* React
+* HMR
+* SCSS
+* React-Router
---
+++ webpack.production.config.js
@@ -0,0 +1,97 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+const { resolve } = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+// TODO update when fixed
+const StyleExtHtmlWebpackPlugin = require('style-ext-html-webpack-plugin-webpack-4');
+
+const config = {
+  stats: {
+    maxModules: 0,
+  },
+  mode: 'production',
+  entry: [
+    './main.js',
+    './assets/scss/main.scss',
+  ],
+  context: resolve(__dirname, 'src'),
+  output: {
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js',
+    path: resolve(__dirname, 'dist'),
+    publicPath: '',
+  },
+
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+      options: {
+        context: __dirname,
+      },
+    }),
+    new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } }),
+    new ExtractTextPlugin({ filename: './styles/style.css', disable: false, allChunks: true }),
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/src/index.html`,
+      filename: 'index.html',
+      inject: 'body',
+    }),
+    new StyleExtHtmlWebpackPlugin(),
+    new ScriptExtHtmlWebpackPlugin({ inline: /\.js$/ }),
+  ],
+  optimization: {
+    runtimeChunk: false,
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+      }),
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            { loader: 'sass-loader', query: { sourceMap: false } },
+          ],
+          publicPath: '../',
+        }),
+      },
+      {
+        test: /\.(jpe?g|png|ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        use: 'base64-inline-loader?limit=1000&name=[name].[ext]',
+      },
+    ],
+  },
+};
+
+module.exports = config;
