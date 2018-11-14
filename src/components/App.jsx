298d6e3419ec8646c863d0929a5a6ef076dcf01e
Chris Hitchcott <hitchcott@gmail.com>
2018-11-14 21:28:25 +0700

    rename invoicer to donate, add invoce route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,43 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import logo from '../assets/images/logo.png';
 
-import Invoicer from './Invoicer';
+import Donate from './Donate';
+import Invoice from './Invoice';
 
 export default () => {
   return (
-    <div id="layout-container">
-      <div id="main-header">
-        <a
-          href="https://lncm.io"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={logo} alt="LNCM" className="logo" />
-        </a>
-        <h1>
-          LN Donation
-          <br />
-          <small>Lightning Network Chiang Mai</small>
-        </h1>
-        <h2 style={{ color: 'red' }}>Warning: This is a demo, do not use significant value</h2>
+    <Router>
+      <div>
+        <div id="nav">
+          <ul>
+            <li><Link to="/">Donte</Link></li>
+            <li><Link to="invoice">Invoice</Link></li>
+          </ul>
+        </div>
+        <div id="layout-container">
+          <div id="main-header">
+            <a
+              href="https://lncm.io"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={logo} alt="LNCM" className="logo" />
+            </a>
+            <h1>
+            LN Invoicer
+              <br />
+              <small>Lightning Network Chiang Mai</small>
+            </h1>
+            <h2 style={{ color: 'red' }}>Warning: This is a demo, do not use significant value</h2>
+          </div>
+          <div className="content">
+            <Route exact path="/" component={Donate} />
+            <Route path="/invoice" component={Invoice} />
+          </div>
+        </div>
       </div>
-      <div className="content">
-        <Invoicer />
-      </div>
-    </div>
+    </Router>
   );
 };
---
+++ src/components/Invoice.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Invoice extends Component {
+  render() {
+    return (
+      <div>Do some magic here</div>
+    );
+  }
+}
