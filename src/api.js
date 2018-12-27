777d8c1e90b1bcae9dd2b8461057f6a5c47c33cc
Another Droog <36770425+AnotherDroog@users.noreply.github.com>
2018-12-27 12:15:53 +0700

    Remove expire

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 import { baseUrl, donationDesc, invoiceDesc, expire } from './config';
 
 export async function newInvoice(amount) {
-  return (await fetch(`${baseUrl}/invoice?amount=${amount}&desc=${invoiceDesc}&expire=${expire}`)).json();
+  return (await fetch(`${baseUrl}/invoice?amount=${amount}&desc=${invoiceDesc}`)).json();
 }
 
 export async function newDonation() {
-  return (await fetch(`${baseUrl}/invoice?&desc=${donationDesc}&expire=${expire}`)).json();
+  return (await fetch(`${baseUrl}/invoice?&desc=${donationDesc}`)).json();
 }
 
 export async function awaitStatus(invoiceId) {
