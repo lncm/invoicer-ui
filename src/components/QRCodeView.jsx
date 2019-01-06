031f9360028e8b052aa87c23e2f6d2999f706ba3
BTCln <g129@protonmail.com>
2019-01-06 17:57:31 -0500

    Improved UI and removed old unused files.

--- src/components/QRCodeView.jsx
+++ src/components/QRCodeView.jsx
@@ -3,13 +3,11 @@ import React, { Component } from 'react';
 import QRCode from 'qrcode.react';
 
 const QRCodeView = ({ invoice }) => {
-  // <QRCode value={uri} renderAs="svg" style={{ width: '583px', height: '583px', maxHeight: '583px' }} />
-
   const uri = `lightning:${invoice}`;
 
   return (
     <div id="qr-main">
-    // <QRCode value={uri} renderAs="svg" style={{ width: '503px', height: '503px', maxHeight: '503px' }} />
+     <QRCode value={uri} renderAs="svg" style={{ width: '503px', height: '503px', maxHeight: '503px' }} />
     </div>
   );
 }
