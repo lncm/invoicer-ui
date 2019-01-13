f9bcb94a432ea880c2de5e15560a9f855d0a1276
Chris Hitchcott <hitchcott@gmail.com>
2019-01-13 16:33:04 +0700

    Add KeyPad component

---
+++ src/components/KeyPad.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import '../assets/scss/keypad.scss';
+import KeyPadButtons from './KeyPadButtons';
+
+export default class KeyPad extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+  handleKeyPress(value) {
+    const { text } = this.state;
+    if (value === 'enter') {
+      // TODO replace with this.props.someAction
+      alert(parseFloat(text));
+      return this.setState({ text: '' });
+    }
+    if (value === 'clear') {
+      return this.setState({ text: '' });
+    }
+    if (value === 'delete') {
+      return this.setState({ text: text.slice(0, -1) });
+    }
+    // ignore invalid input
+    if (
+      (text.length > 15)
+      || (value.indexOf('0') === 0 && text === '')
+      || (value === '.' && (text === '' || text.indexOf('.') > -1))
+    ) {
+      return null;
+    }
+    return this.setState({ text: text + value });
+  }
+  render() {
+    const { text } = this.state;
+    return (
+      <div className="keypad">
+        <div className="keypad-display">{text || ' '}</div>
+        <KeyPadButtons onKeyPress={this.handleKeyPress} />
+      </div>
+    );
+  }
+}
---
+++ src/components/KeyPadButtons.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+export default class KeyPadButtons extends Component {
+  shouldComponentUpdate() {
+    return false;
+  }
+  renderButton(value, { content, className = '' } = {}) {
+    const { onKeyPress } = this.props;
+    return (
+      <div
+        role="button"
+        className={`keypad-button ${className}`}
+        onClick={() => onKeyPress(value)}
+      >
+        {content || value}
+      </div>
+    );
+  }
+  render() {
+    return (
+      <div className="keypad-buttons">
+        {this.renderButton('7')}
+        {this.renderButton('8')}
+        {this.renderButton('9')}
+        {this.renderButton('clear', { content: 'Clear' })}
+        {this.renderButton('4')}
+        {this.renderButton('5')}
+        {this.renderButton('6')}
+        {this.renderButton('delete', { content: 'Delete' })}
+        {this.renderButton('1')}
+        {this.renderButton('2')}
+        {this.renderButton('3')}
+        {this.renderButton('0')}
+        {this.renderButton('000')}
+        {this.renderButton('.')}
+        {this.renderButton('enter', { content: 'OK', className: 'enter' })}
+      </div>
+    );
+  }
+}
