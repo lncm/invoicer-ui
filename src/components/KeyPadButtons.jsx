da56f73ed00359c2540cc9a05fae17ecbe93d375
BTCln <g129@protonmail.com>
2019-01-19 13:54:43 -0500

    UI changes to Keypad buttons, on Enter Amount screen

--- src/components/KeyPadButtons.jsx
+++ src/components/KeyPadButtons.jsx
@@ -22,16 +22,16 @@ export default class KeyPadButtons extends Component {
         {this.renderButton('7')}
         {this.renderButton('8')}
         {this.renderButton('9')}
-        {this.renderButton('clear', { content: 'Clear' })}
+        {this.renderButton('clear', { content: 'Clear', className: 'clear'})}
         {this.renderButton('4')}
         {this.renderButton('5')}
         {this.renderButton('6')}
-        {this.renderButton('delete', { content: 'Delete' })}
+        {this.renderButton('delete', { content: 'Delete', className: 'delete'})}
         {this.renderButton('1')}
         {this.renderButton('2')}
         {this.renderButton('3')}
         {this.renderButton('0')}
-        {this.renderButton('000')}
+        {this.renderButton('00')}
         {this.renderButton('.')}
         {this.renderButton('enter', { content: 'OK', className: 'enter' })}
       </div>
