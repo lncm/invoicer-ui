e72de19f2c857f621890ea4cb98704caaf54f0cc
nolim1t <hello@nolim1t.co>
2020-10-14 21:36:25 +0700

    Add QR Code view

--- src/components/QRCodeView.jsx
+++ src/components/QRCodeView.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import QRCode from 'qrcode.react';
 import PropTypes from 'prop-types';
+import LNInvoiceWindow from './LNInvoiceWindow';
 
 const QRCodeView = ({ address, amount, bolt11, qrCodeType }) => {
   let uri = `bitcoin:${address}?amount=${amount}&lightning=${bolt11}`;
@@ -10,9 +11,23 @@ const QRCodeView = ({ address, amount, bolt11, qrCodeType }) => {
     uri = `lightning:${bolt11}`;
   }
 
+  function clickTest() {
+    //document.write(bolt11);
+    console.log("copying bolt11 to clipboard")
+    document.getElementById("bolt11value").value = bolt11;
+    console.log(document.getElementById("bolt11value").value);
+    //document.execCommand("copy");
+  }
+
   return (
     <div id="qr-main">
-      <QRCode value={uri} renderAs="svg" style={{ width: '503px', height: '503px', maxHeight: '503px' }} />
+      <div id="bolt11value" style={{ hidden: true }}></div>
+      <div>
+      <QRCode value={uri} onClick={clickTest} renderAs="svg" style={{ width: '320px', height: '430px', maxHeight: '420px' }} />
+      </div>
+      <div>
+      <LNInvoiceWindow bolt11={bolt11} />
+      </div>
     </div>
   );
 };
@@ -23,5 +38,6 @@ QRCodeView.propTypes = {
   bolt11: PropTypes.string.isRequired,
   qrCodeType: PropTypes.string.isRequired,
 };
+
 
 export default QRCodeView;
